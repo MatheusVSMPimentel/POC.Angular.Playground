34aa91d351ff5350f8cdb9cfd29d744f32945468
Matheus Pimentel <matheusvsm.pimentel@hotmail.com>
2024-07-08 08:28:40 -0300

    chore: import styles chore: create routes and component to registration page feat: adjust Angular.json to create html file when Angular generates component

---
+++ src/app/demos/reactiveForms/registration/registration.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './registration.component.html',
+  styles: ``
+})
+export class RegistrationComponent {
+
+}
