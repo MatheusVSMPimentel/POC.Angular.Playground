a2e79acb31001630911ef4508e2d66d75a8c77ec
Matheus Pimentel <matheusvsm.pimentel@hotmail.com>
2024-07-07 21:29:00 -0300

    feat observable with observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,40 +14,53 @@ import { Observable, subscribeOn } from 'rxjs';
   styles: [],
 })
 export class AppComponent implements OnInit {
-  
+
   title = 'RXJS';
 
   ngOnInit(): void {
     //this.myPromise('Eduardo').then(result=> console.log(result)).catch(rejected => console.log(rejected))
-    this.myObservable("").subscribe({
+
+/*     this.myObservable("").subscribe({
       next: (v) => console.log(v),
       error: (e) => console.error(e),
-      complete: () => console.info('complete') 
-  });
-  
+      complete: () => console.info('complete')
+    }); */
+
+    const observer = {
+      next: (valor: any) => console.log('Next: ', valor),
+      error: (erro: any) => console.log('Erro: ', erro),
+      complete: () => console.info('complete')
+
+    }
+
+    const obs = this.myObservable('Matheus');
+
+    obs.subscribe(observer);
+
   }
 
-  myObservable(name: string) : Observable<string>{
+  myObservable(name: string): Observable<string> {
     return new Observable(subscriber => {
-      if (name === "Matheus"){
+      if (name === "Matheus") {
         subscriber.next("Ola! " + name);
-        subscriber.next("Ola! de novo "+ name);
-        setTimeout(()=>{
+        subscriber.next("Ola! de novo " + name);
+        setTimeout(() => {
           subscriber.next("Resposta com delay. " + name);
-        }, 5000)
-      }else{
+        }, 5000);
+        subscriber.complete();
+      } else {
         subscriber.error('Ops! Você não é o Matheus.');
       }
     })
   }
 
-  myPromise(name: string): Promise<string>{
-    return new Promise((resolve, reject )=>{
-      if (name === "Matheus"){
-        setTimeout(()=>{
+  myPromise(name: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      if (name === "Matheus") {
+        setTimeout(() => {
           resolve("Welcome " + name);
         }, 1000)
-      }else{
+      } else {
         reject('Ops! Você não é o Matheus.');
       }
     })
