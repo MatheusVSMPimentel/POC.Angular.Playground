fa139ff1898393367585aa157cc66a3791f885c7
Matheus Pimentel <matheusvsm.pimentel@hotmail.com>
2024-07-08 13:29:28 -0300

    feat: form control first way

--- src/app/demos/reactiveForms/registration/registration.component.ts
+++ src/app/demos/reactiveForms/registration/registration.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -8,6 +8,29 @@ import { ReactiveFormsModule } from '@angular/forms';
   templateUrl: './registration.component.html',
   styles: ``
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+  
+  registerForm: FormGroup = new FormGroup({});
 
+  ngOnInit(){
+    this.registerForm = new FormGroup({
+      name : new FormControl(''),
+      document : new FormControl(''),
+      email : new FormControl(''),
+      password : new FormControl(''),
+      passwordConfirm : new FormControl(''),
+    }) 
+
+
+
+    /* let name = new FormControl('');
+    let document = new FormControl('');
+    let email = new FormControl('');
+    let password = new FormControl('');
+    let passwordConfirm = new FormControl(''); */
+  }
+
+  addUser(){
+    let x = this.registerForm.value;
+  }
 }
