60a82d05910bf671286b8c63a86600b95ab53b97
Matheus Pimentel <matheusvsm.pimentel@hotmail.com>
2024-07-21 18:08:50 -0300

    feature: navigation dynamic feature: not found component

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from "@angular/core";
 import { MenuComponent } from "./menu/menu.component";
 import { HomeComponent } from "./home/home.component";
 import { FooterComponent } from "./footer/footer.component";
-import { CommonModule } from "@angular/common";
+import { CommonModule, NgFor } from "@angular/common";
 import { RouterLink, RouterModule } from "@angular/router";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 @NgModule({
-    declarations: [MenuComponent,HomeComponent,FooterComponent],
+    declarations: [MenuComponent,HomeComponent,FooterComponent, NotFoundComponent],
     imports: [
         CommonModule,
         RouterModule,
-        RouterLink
+        RouterLink, NgFor
     ],
-    exports:[MenuComponent,HomeComponent,FooterComponent]
+    exports:[MenuComponent,HomeComponent,FooterComponent, NotFoundComponent]
 })
 export class NavigationModule{
 
