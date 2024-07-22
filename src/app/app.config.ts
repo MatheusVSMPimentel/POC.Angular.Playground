d38392ae0663a2aaec790dde25ed9f654005b4bb
Matheus Pimentel <matheusvsm.pimentel@hotmail.com>
2024-07-21 23:41:45 -0300

    chore: add registerLocaleData to PT locale into app config to spread all over the app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,9 @@ import { QuestionService } from './demos/dynamicForms/question/question.service'
 import { AuthGuard } from './services/app.guard';
 import { RegisterGuard } from './services/register.guard';
 
+import  localePt  from "@angular/common/locales/pt";
+import { registerLocaleData } from "@angular/common";
+registerLocaleData(localePt)
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
