9bb88c77ae723d2d40fdd3267d3953f541b6bf03
Matheus Pimentel <matheusvsm.pimentel@hotmail.com>
2024-07-28 10:57:44 -0300

    feature: create counter component test: create unit test for counter component

--- src/app/demos/todo-list/todo.component.ts
+++ src/app/demos/todo-list/todo.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { TaskListComponent } from './components/task-list/task-list.component';
-import { ToDoTasksComponent } from './components/to-do-tasks/to-do-tasks.component';
-import { DoingTasksComponent } from './components/doing-tasks/doing-tasks.component';
 
 @Component({
   selector: 'app-todo',
--- src/app/demos/todo-list/todo.module.ts
+++ src/app/demos/todo-list/todo.module.ts
@@ -1,25 +1,28 @@
-import { CommonModule, NgFor, NgIf } from "@angular/common";
+import { CommonModule, NgClass, NgFor, NgIf } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { CustomFormsModule } from "ngx-custom-validators";
 import { TasksService } from "./todo.service";
+import { Store } from "./todo.store";
 import { TodoComponent } from "./todo.component";
 import { ToDoTasksComponent } from "./components/to-do-tasks/to-do-tasks.component";
 import { DoingTasksComponent } from "./components/doing-tasks/doing-tasks.component";
 import { FinishedTasksComponent } from "./components/finished-tasks/finished-tasks.component";
 import { TaskListComponent } from "./components/task-list/task-list.component";
-import { Store } from "./todo.store";
 import { TaskFormComponent } from "./components/task-form/task-form.component";
-import { CustomFormsModule } from "ngx-custom-validators";
 import { ReactiveFormsModule } from "@angular/forms";
 
+
 @NgModule({
+    
     imports:[
         CommonModule, 
         NgIf,
         NgFor,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        NgClass
     ],
     providers:[
-        CustomFormsModule,TasksService, { provide: Store, useClass: Store }
+        CustomFormsModule, TasksService,  { provide: Store, useClass: Store }
     ],
     declarations:[
         TodoComponent,
@@ -34,8 +37,6 @@ import { ReactiveFormsModule } from "@angular/forms";
         FinishedTasksComponent,
         DoingTasksComponent,
         TaskListComponent,
-        TaskFormComponent
-
     ]
 })
 export class ToDoModule{}
